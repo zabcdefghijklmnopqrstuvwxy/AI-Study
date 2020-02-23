fb788e60be794e1e8dee8232d7ca16a45c05a88d
sai <sai14790085178@163.com>
2020-02-23 12:31:12 +0800

    2020-02-23 sai +)[1.numpy]增加topic40《创建一个大小为10的随机向量并对其进行排序》，测试通过

---
+++ 1.numpy/topic40/readme.txt
@@ -0,0 +1,2 @@
+描述：Create a random vector of size 10 and sort it
+创建一个大小为10的随机向量并对其进行排序
---
+++ 1.numpy/topic40/topic40.py
@@ -0,0 +1,5 @@
+import numpy as np 
+arr=np.random.randint(1,10,size=(1,10))
+print(f"arr random is {arr}")
+arr.sort()
+print(f"arr sort is {arr}")
