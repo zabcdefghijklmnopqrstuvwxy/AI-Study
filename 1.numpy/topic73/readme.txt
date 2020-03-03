05b0f0b44bbc52be185a05fc5689a895f3e70492
sai <sai14790085178@163.com>
2020-03-03 22:06:29 +0800

    2020-03-03 sai +)[1.numpy]增加topic73《考虑一组描述10个三角形（具有共享顶点）的10个三元组，找到组成所有三角形的唯一线段的集合》，测试通过

---
+++ 1.numpy/topic73/readme.txt
@@ -0,0 +1,2 @@
+描述： Consider a set of 10 triplets describing 10 triangles (with shared vertices), find the set of unique line segments composing all the  triangles
+考虑一组描述10个三角形（具有共享顶点）的10个三元组，找到组成所有三角形的唯一线段的集合 
---
+++ 1.numpy/topic73/topic73.py
@@ -0,0 +1,9 @@
+import numpy as np 
+
+faces = np.random.randint(0,100,(10,3))
+F = np.roll(faces.repeat(2,axis=1),-1,axis=1)
+F = F.reshape(len(F)*3,2)
+F = np.sort(F,axis=1)
+G = F.view( dtype=[('p0',F.dtype),('p1',F.dtype)] )
+G = np.unique(G)
+print(G)
