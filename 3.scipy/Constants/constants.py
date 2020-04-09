cd683a1d6fb3d4cf90f2a1daa8e071ae518b2716
sai <sai14790085178@163.com>
2020-04-09 20:29:17 +0800

    2020-04-09 sai +)[3.scipy]增加Constants常量

---
+++ 3.scipy/Constants/constants.py
@@ -0,0 +1,12 @@
+from scipy import constants as C  
+
+print(f"C.c={C.c}")  #真空中的光速
+print(f"C.h={C.h}")  #普朗克常数
+
+print(f"C.mile={C.mile}")   #1英里等于多少米
+print(f"C.inch={C.inch}")   #1英寸等于多少米
+print(f"C.gram={C.gram}")   #1克等于多少千克
+print(f"C.pound={C.pound}")  #1磅等于多少千克
+
+print(f"C.pi={C.pi}")     #π值
+print(f"C.g={C.g}")      #标准重力加速度
